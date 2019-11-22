0ae5dbe9a288049fe37d3b2b97745398935fea9c
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-11-22 13:10:14 +0530

    Initial Commit

---
+++ handler.py
@@ -0,0 +1,48 @@
+import os
+from sandbox import Sandbox
+import string
+import random
+import uuid
+
+compiler_dict = {
+    'python': ["python", "file.py", "", "Python", ""],
+    'C/C++': ["\'g++ -o /usercode/a.out\' ", "file.cpp", "/usercode/a.out",
+              "C/C++", ""]
+}
+
+
+def handler(event, context):
+    language = event['request']['language']
+    code = event['request']['code']
+    stdin = event['request']['stdin']
+    return compile_code(language, code, stdin)
+
+
+def compile_code(language, code, stdin):
+
+    temp_folder = os.path.join('/tmp', str(uuid.uuid4()))
+    timeout_value = 20
+    path = os.getcwd()
+
+    sandbox = Sandbox(
+        timeout_value=timeout_value,
+        path=path,
+        temp_folder=temp_folder,
+        compiler_name=compiler_dict[language][0],
+        file_name=compiler_dict[language][1],
+        code=code,
+        output_command=compiler_dict[language][2],
+        language_name=compiler_dict[language][3],
+        e_arguments=compiler_dict[language][4],
+        stdin_data=stdin
+    )
+
+    (data, exec_time, err) = sandbox.run()
+
+    return {
+        "output":data,
+        "langid": language,
+        "code":code,
+        "errors":err,
+        "time":exec_time
+    }
---
+++ sandbox.py
@@ -0,0 +1,73 @@
+import os
+from shutil import copy, rmtree
+import time
+
+
+class Sandbox:
+    def __init__(
+            self, timeout_value, path, temp_folder, compiler_name,
+            file_name, code, output_command, language_name, e_arguments,
+            stdin_data
+    ):
+        self.timeout_value = timeout_value
+        self.path = path
+        self.temp_folder = temp_folder
+        self.compiler_name = compiler_name
+        self.file_name = file_name
+        self.code = code
+        self.output_command = output_command
+        self.language_name = language_name
+        self.e_arguments = e_arguments
+        self.stdin_data = stdin_data
+
+    def prepare(self):
+
+        os.makedirs(self.temp_folder)
+
+        src_payload_path = os.path.join(self.temp_folder, "Payload")
+        os.makedirs(src_payload_path)
+
+        code_file_path = os.path.join(src_payload_path, self.file_name)
+        f = open(code_file_path, 'w')
+        f.write(self.code)
+        f.close()
+        os.chmod(code_file_path, mode=0o777)
+
+        input_file_path = os.path.join(self.temp_folder, "inputFile")
+        f = open(input_file_path, 'w')
+        f.write(self.stdin_data)
+        f.close()
+        os.chmod(input_file_path, mode=0o777)
+
+    def execute(self):
+        logfile_path = os.path.join(self.temp_folder, "logfile.txt")
+        completed_file_path = os.path.join(self.temp_folder, "completed")
+        errors_file_path = os.path.join(self.temp_folder, "errors")
+
+        script_path = os.path.join(self.path, 'Payload/script.sh')
+
+        command_str = "sh {script_path} {temp_folder} {compiler_name}".format(
+            script_path=script_path, temp_folder=self.temp_folder,
+            compiler_name=self.compiler_name, )
+        os.system(command_str)
+
+        f = open(completed_file_path, 'r')
+        completed_data = f.read()
+        f.close()
+
+        data = completed_data.split('*-COMPILEBOX::ENDOFOUTPUT-*')[0]
+        exec_time = completed_data.split('*-COMPILEBOX::ENDOFOUTPUT-*')[1]
+
+        if not os.path.exists(errors_file_path):
+            errors_data = ""
+        else:
+            f = open(errors_file_path, 'r')
+            errors_data = f.read()
+            f.close()
+
+        rmtree(self.temp_folder)
+        return data, exec_time, errors_data
+
+    def run(self):
+        self.prepare()
+        return self.execute()
